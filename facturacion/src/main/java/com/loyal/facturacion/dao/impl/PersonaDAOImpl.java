8f8cd109f75b28eeb87894cebdf4422c93515ad5
pjuncos <pablojuncos@wengan.com.ar>
2014-02-06 12:15:11 -0300

    Cambio query de personas

--- facturacion/src/main/java/com/loyal/facturacion/dao/impl/PersonaDAOImpl.java
+++ facturacion/src/main/java/com/loyal/facturacion/dao/impl/PersonaDAOImpl.java
@@ -8,10 +8,12 @@ import java.util.Map;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.loyal.facturacion.dao.PersonaDAO;
 import com.loyal.facturacion.model.Persona;
+import com.loyal.facturacion.model.Rol;
 
 public class PersonaDAOImpl extends JdbcDaoSupport implements PersonaDAO {
 
@@ -19,14 +21,15 @@ public class PersonaDAOImpl extends JdbcDaoSupport implements PersonaDAO {
 	@Override
 	@Transactional
 	public void insert(Persona persona) {
+		MessageDigestPasswordEncoder m = new MessageDigestPasswordEncoder("MD5");
 		String sql = "INSERT INTO personas "
-				+ "(NOMBRE, APELLIDO, USERNAME, MAIL, TELEFONO, PASSWORD, ACCESS_ENABLED) VALUES (?, ?, ?, ?, ?, ?, ?)";
+				+ "(NOMBRE, APELLIDO, USERNAME, MAIL, TELEFONO, PASSWORD, ACCESS_ENABLED, ROLE_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 		getJdbcTemplate().update(
 				sql,
 				new Object[] { persona.getNombre(), persona.getApellido(),
 						persona.getUsername(), persona.getMail(),
-						persona.getTelefono(), persona.getPassword(),
-						persona.isEnabled() });
+						persona.getTelefono(),  m.encodePassword(persona.getPassword(), null) ,
+						persona.isEnabled(), persona.getRol().getId() });
 	}
 
 	@Override
@@ -40,7 +43,9 @@ public class PersonaDAOImpl extends JdbcDaoSupport implements PersonaDAO {
 	@Override
 	public List<Persona> getAll() {
 
-		String sql = "SELECT PERSONA_ID, NOMBRE, APELLIDO, USERNAME, TELEFONO, ACCESS_ENABLED FROM personas";
+		String sql = "SELECT PERSONA_ID, NOMBRE, APELLIDO, USERNAME, TELEFONO, ACCESS_ENABLED, p.ROLE_ID, ROLE, DESCRIP "
+				+ "FROM personas p, roles r "
+				+ "WHERE p.ROLE_ID = r.ROLE_ID;";
 
 		List<Persona> lista = new ArrayList<Persona>();
 
@@ -53,6 +58,11 @@ public class PersonaDAOImpl extends JdbcDaoSupport implements PersonaDAO {
 			persona.setUsername((String)row.get("USERNAME"));
 			persona.setTelefono((String)row.get("TELEFONO"));
 			persona.setEnabled((Boolean)row.get("ACCESS_ENABLED")) ;
+			Rol rol = new Rol();
+			rol.setId(Integer.parseInt(String.valueOf(row.get("ROLE_ID"))));
+			rol.setDescripcion((String)row.get("DESCRIP"));
+			rol.setRole((String)row.get("ROLE"));
+			persona.setRol(rol);
 			lista.add(persona);
 		}
 
@@ -84,6 +94,11 @@ public class PersonaDAOImpl extends JdbcDaoSupport implements PersonaDAO {
 			persona.setUsername(rs.getString("USERNAME"));
 			persona.setTelefono(rs.getString("TELEFONO"));
 			persona.setEnabled(rs.getBoolean("ACCESS_ENABLED")) ;
+			Rol rol = new Rol();
+			rol.setId(rs.getInt("ROLE_ID"));
+			rol.setDescripcion(rs.getString("DESCRIP"));
+			rol.setRole(rs.getString("ROLE"));
+			persona.setRol(rol);
 			return persona;
 		}
 	}
